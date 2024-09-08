b97d80f8f3923b15ae47d147737124d48740b5c5
KM Manish <156883335+Manish50518@users.noreply.github.com>
2024-09-08 16:41:56 +0530

    commit

---
+++ src/useFetch.js
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const abortCont = new AbortController();
+
+    setTimeout(() => {
+      fetch(url, { single: abortCont.single })
+        .then((res) => {
+          if (!res.ok === true) {
+            throw Error(
+              "could not fetch the data from the resourse 😞😞 Try again..."
+            );
+          }
+
+          return res.json();
+        })
+        .then((data) => {
+          setData(data);
+          setIsPending(false);
+          setError(null);
+        })
+        .catch((err) => {
+          if (err.name === "AbortError") {
+            console.log("fetch aborted");
+          } else {
+            setIsPending(false);
+            setError(err.message);
+          }
+        });
+    }, 1000);
+
+    return () => abortCont.abort();
+  }, [url]);
+
+  return { data, isPending, error };
+};
+
+export default useFetch;
